7b137ab6f5f0721707a210833c12f095453f9503
Lorenzo Destriere <lorenzo.destriere@gmail.com>
2023-07-01 12:54:54 +0200

    build: migliora lo script per la cattura dei dati

--- captureCPUAndMemData/docker-stats.sh
+++ captureCPUAndMemData/docker-stats.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+header="CONTAINER ID   NAME                                       CPU %     MEM USAGE / LIMIT     MEM %     NET I/O         BLOCK I/O       PIDS"
+echo "$header" > stats.csv
+
 while true; do
-    docker stats --no-stream | { echo; cat; } | tee --append stats.csv
+    stats_output=$(docker stats --no-stream | tail -n 2)
+    echo "$stats_output" >> stats.csv
     echo >> stats.csv
     sleep 1
 done
